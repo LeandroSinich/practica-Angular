02a6a790491ae8415d781d0e6b09e12cfe8d46d1
LeandroSinich <leandrosinich@gmail.com>
2024-06-18 17:27:00 -0300

    practica de routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { FormsModule } from '@angular/forms';
 import { HermanoComponent } from './hermano/hermano.component';
 import { BlackBackDirective } from './black-back.directive';
 import { MiPipePipe } from './mi-pipe.pipe';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,12 @@ import { MiPipePipe } from './mi-pipe.pipe';
     HijoComponent,
     HermanoComponent,
     BlackBackDirective,
-    MiPipePipe
+    MiPipePipe,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/black-back.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlackBackDirective } from './black-back.directive';
-
-describe('BlackBackDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BlackBackDirective();
-    expect(directive).toBeTruthy();
-  });
-});
